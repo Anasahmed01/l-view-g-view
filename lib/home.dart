4810abe625d56f06ef8d718307ae8c19efef5017
anasahmed01 <zainkhilji2323@gmail.com>
2022-03-23 16:50:54 +0500

    drawar_add

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,44 @@ class Homeapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("MyApp"),
+        titleTextStyle: const TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       body: GridView.count(
         crossAxisCount: 2,
         children: [
