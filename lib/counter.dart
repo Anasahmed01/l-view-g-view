057acd161b89cd69c6ed3019b902ff9eda016fd6
anasahmed01 <zainkhilji2323@gmail.com>
2022-06-11 20:28:40 +0500

    add_counter

---
+++ lib/counter.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int abc = 0;
+
+  increamentValue() {
+    setState(() {
+      if (abc < 10) {
+        ++abc;
+      }
+    });
+    print(abc);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$abc"),
+            ElevatedButton(
+                onPressed: () {
+                  increamentValue();
+                },
+                child: Text('add'))
+          ],
+        ),
+      ),
+    );
+  }
+}
